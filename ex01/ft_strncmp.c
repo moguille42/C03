648e04cf2fcb48e58bd70ca00437fb2d7141f6e2
moguille <moguille@student.42madrid.com>
2025-03-09 15:27:17 +0100

    ex01

--- ex01/ft_strncmp.c
+++ ex01/ft_strncmp.c
@@ -6,50 +6,37 @@
 /*   By: moguille <moguille@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/08 18:04:45 by moguille          #+#    #+#             */
-/*   Updated: 2025/03/08 21:15:38 by moguille         ###   ########.fr       */
+/*   Updated: 2025/03/09 15:21:46 by moguille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
-#include <stdio.h>
-
 int	ft_strncmp(char *s1, char *s2, unsigned int n)
-
 {
-	while (n > 0)
+	unsigned int	i;
+
+	i = 0;
+	while (i < n && s1[i] != '\0' && s1[i] == s2[i])
 	{
-		if (*s1 == '\0' && (*s1 != *s2))
-			s1++;
-			s2++;
-			n--;
-	return ((unsigned char)*s1 - (unsigned char)*s2);
+		i++;
 	}
-	return (0);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
 
+/*#include <string.h>
+#include <stdio.h>
 int	main(void)
 {
-	char	s1[] = 
-	char 	s2[] = 
-	s1[] = "viñedo";
-	s2[5] = "viña";
-
+	char	s1[] = "lluvioso";
+	char 	s2[] = "lluvia";
 	int res;
-	res = 0;
+	int size =3;
 
-	res = ft_strncmp(s1, s2, 4);
-
+	res = ft_strncmp(s1, s2, size);
 	if (res == 0)
-	{
-		printf("las primeras 4 letras son iguales.\n");
-	}
+		printf("las primeras %d letras son iguales.\n", size);
 	else if (res < 0)
-	{
 		printf("<str1> is less.\n");
-	}
 	else
-	{
 		printf("<str1> is greater. \n");
-	}
 	return (0);
-}
+}*/
